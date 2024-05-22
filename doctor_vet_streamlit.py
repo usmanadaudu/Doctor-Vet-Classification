c40b0c9ba9d76161cbbcb816ce27a66a2362be5d
usmanadaudu <adekunleakorede101@gmail.com>
2024-05-22 07:29:36 +0100

    fixing bug

--- doctor_vet_streamlit.py
+++ doctor_vet_streamlit.py
@@ -6,7 +6,8 @@ import numpy as np    # for mathematical calculations
 import pandas as pd   # for working with structured data (dataframes)
 import streamlit as st
 from xgboost import XGBClassifier                 # XGBoost model
-from nltk.corpus import stopwords                 # for getting stopwords
+# from nltk.corpus import stopwords                 # for getting stopwords
+import nltk
 from sqlalchemy import create_engine              # for connecting to database
 from sklearn.metrics import accuracy_score        # for getting prediction accuracy
 from sklearn.naive_bayes import MultinomialNB     # Multinimial Naive Bayes model
@@ -139,7 +140,7 @@ for the preprocessing, the various steps that would be done are:\n
 2.  emoving file directories\n
 3.  Removing deleted comments indicated as'[deleted]'
 4.  Removing stopwords. Stopwords are:""")
-st.code(stopwords.words('english'), language="text")
+st.code(nltk.corpus.stopwords.words('english'), language="text")
 st.write("""
 5.  Removing punctuations
 6.  Removing non-alphabetic characters
